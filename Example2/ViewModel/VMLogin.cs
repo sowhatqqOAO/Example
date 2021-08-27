5bf6ff94886957487e019451494b52eaa32fbad1
Tim <sowhatqq02@gmail.com>
2021-08-27 18:02:01 +0800

    加入登入頁面

---
+++ Example2/ViewModel/VMLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Example2.ViewModel
+{
+    public class VMLogin
+    {
+        [Display(Name = "帳號")]
+        public string Account { get; set; }
+
+        [Display(Name = "密碼")]
+        public string Password { get; set; }
+    }
+}
