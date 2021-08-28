d08fdde5257ff6491fc2b2c9aeb7b80243c4f70c
tim_shen <sowhatqq02@yahoo.com.tw>
2021-08-28 11:30:53 +0800

    套用CODE FIRST 引用Entity Framework WebConfig新增db連線字串

---
+++ Example2/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Example2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Example2.Model.ETContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Example2.Model.ETContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Example2/Model/ETContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Example2.Model
+{
+    /// <summary>
+    /// 初始化：name=>WebConfig 資料庫連線字串名稱
+    /// </summary>
+    public class ETContext : DbContext
+    {
+        /// <summary>
+        /// 初始化：name=>WebConfig 資料庫連線字串名稱
+        /// </summary>
+        public ETContext() : base("name=DefaultConnectionString") { }
+        //public DbSet<Sys_Module> Sys_Module { get; set; }//模組
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //避免Code First建立Table時將Table Name加入複數名稱(s)
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
